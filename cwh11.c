5d369feab44020231544c503e71bf66f6f52f760
Harman Kajla <harmankajla209@gmail.com>
2021-05-06 00:58:49 +0530

    completed cwh11.c

--- cwh11.c
+++ cwh11.c
@@ -2,6 +2,29 @@
 
 int main()
 {
+    int age;
+    printf("Enter Your Age\n");
+    scanf("%d", &age);
+
+
+    switch (age)
+    {
+    case 3:
+        printf("Age is 3\n");
+        break;
+        
+    case 13:
+        printf("Age is 13\n");
+        break;
+
+    case 23:
+        printf("Age is 23\n");
+        break;
+    
+    default:
+        printf("Age is not 3, 13 or 23\n");
+        break;
+    }
 
     return 0;
 }
