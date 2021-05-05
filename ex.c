1fcf0e7c46042a7ce0c0cbbfff98ca5034807af9
Harman Kajla <harmankajla209@gmail.com>
2021-05-06 00:39:12 +0530

    First Commit

---
+++ ex.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int sub;
+    printf("What subject you passed\n");
+    scanf("%d", &sub);
+
+    if (sub==2)
+    {
+        printf("You won 15 rupees");
+    }
+
+    if (sub==1)
+    {
+        printf("You won 45");
+    }
+    
+        if (sub==1)
+    {
+        printf("You won 45");
+    }
+    
+    
+
+    return 0;
+}
